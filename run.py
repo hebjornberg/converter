137f8b6abdf6f5a9014147f199c11fe2a0be932f
Helena Bjornberg <helena.bjornberg@hotmail.com>
2024-04-11 16:41:42 +0000

    Add dictionary of temperature with formulas for conversion

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def convert_unit(value, from_unit, to_unit, category):
+    conversions = {
+        "temperature": {
+            "celsius": {
+                "fahrenheit": lambda x: (x * 9/5) + 32, 
+                "kelvin": lambda x: x + 273.15
+            },
+            "fahrenheit": {
+                "celsius": lambda x: (x - 32) * 5/9, 
+                "kelvin": lambda x: (x + 459.67) * 5/9
+            },
+            "kelvin": {
+                "celsius": lambda x: x - 273.15,
+                "fahrenheit": lambda x: (x * 9/5) - 459.67
+            }
+        }
+    }
+    
+    print(conversions)
+
+convert_unit(0, "celsius", "fahrenheit", "temperature")
